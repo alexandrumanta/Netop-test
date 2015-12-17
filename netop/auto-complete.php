87966959c2714c8e3502efe31b417db1a6697019
alexandrumanta <alexandrumanta@yahoo.com>
2015-12-17 20:32:41 +0200

    autocomplete

--- netop/auto-complete.php
+++ netop/auto-complete.php
@@ -1,51 +1,34 @@
 <?php
 require("config/settings.php");
-$book = new Books();                  
-        $books = $book->getBooks();
-foreach ($books as $book) {
-	$category = new Category(); 
+if(isset($_GET['keyword']) && (!empty($_GET['keyword'])) ){
+	try{
+		$value = $_GET['keyword'];
+		$book = new Books;
+		$books = $book->searchKeyword($value);
+		foreach ($books as $book) {
+			$category = new Category(); 
             $category = $category->getCategory($book->category_id);
-	$data[] = array(
-				'id' => $book->id,
-				'name' => $book->name,
-				'author' => $book->author,
-				'image' => $book->image ,
-				'category' => $category[0]->name,
-				'price' => $book->price
+			$data[] = array(
+				'name' => 'Nume:' . $book->name,
+				'author' => 'Autor: ' . $book->author,
+				'image' => '<img src ="' .$book->image . '"/>',
+				'category' => 'Categorie: ' . $category[0]->name,
+				'error' => ''
 				);
+		}
+	}catch (Exception $e){
+		$data[] = array(
+			'error' => $e->getMessage(),
+			'name' => '',
+			'author' => '',
+			'image' => '',
+			'category' => ''
+			);
+	}
+	echo json_encode($data);
+}else{
+	$errors[] = "Eroare la conexiune!";;
+	echo json_encode($data);
 }
-$data = json_encode($data, true);
-echo $data;
-
-// if(isset($_GET['keyword']) && (!empty($_GET['keyword'])) ){
-// 	try{
-// 		$value = $_GET['keyword'];
-// 		$book = new Books;
-// 		$books = $book->searchKeyword($value);
-// 		foreach ($books as $book) {
-// 			$category = new Category(); 
-//             $category = $category->getCategory($book->category_id);
-// 			$data[] = array(
-// 				'name' => 'Nume:' . $book->name,
-// 				'author' => 'Autor: ' . $book->author,
-// 				'image' => '<img src ="' .$book->image . '"/>',
-// 				'category' => 'Categorie: ' . $category[0]->name,
-// 				'error' => ''
-// 				);
-// 		}
-// 	}catch (Exception $e){
-// 		$data[] = array(
-// 			'error' => $e->getMessage(),
-// 			'name' => '',
-// 			'author' => '',
-// 			'image' => '',
-// 			'category' => ''
-// 			);
-// 	}
-// 	echo json_encode($data);
-// }else{
-// 	$errors[] = "Eroare la conexiune!";;
-// 	echo json_encode($data);
-// }
 
 ?>
